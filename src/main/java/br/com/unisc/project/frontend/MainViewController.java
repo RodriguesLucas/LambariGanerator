7700cd220e7877e746728ad67d1278035f5995d3
Lucas Rodrigues <lucasrodrigues4@mx2.unisc.br>
2023-11-02 17:26:41 -0300

    firts commit

---
+++ src/main/java/br/com/unisc/project/frontend/MainViewController.java
@@ -0,0 +1,18 @@
+package br.com.unisc.project.frontend;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainViewController implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try {
+            //new TelaInicial();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
